d1c64094567bb084b618955ccafc6d876ca31734
ratensh <ratnesh@winlab.rutgers.edu>
2016-09-22 18:10:39 -0400

    added metadata change

--- examples/test_tx.cpp
+++ examples/test_tx.cpp
@@ -16,9 +16,9 @@ using namespace win;
 
 double freq = 2e9;
 double sample_rate = 10e6;
-double tx_gain = 30;
+double tx_gain = 20;
 //double rx_gain = 30;
-double amp = 0.5;
+double amp = 0.1;
 std::string device_addr = "addr=192.168.10.2";
 
 char pnseq[PNSEQLEN];
@@ -53,7 +53,7 @@ int main(int argc, char * argv[]){
     for (int i = 0; i<payload.size(); i++)
     {
         // samples.push_back(0.5, 0.0);
-        samples[i] = 0.05*std::complex<double>((2*(double)payload[i])-1, 0.0);
+        samples[i] = amp*std::complex<double>((2*(double)payload[i])-1, 0.0);
     }
 
     tx.init_usrp();
@@ -61,8 +61,11 @@ int main(int argc, char * argv[]){
     tx.reset_usrp_time();
 
     int tx_count = 0;
+    uhd::time_spec_t offset_time = uhd::time_spec_t(0.0);
     while(1)
     {
+        offset_time += uhd::time_spec_t(0.1);
+        tx.set_txmetadata(true, true, true, offset_time);
         tx.send_data(samples);
     }
 }
